ab1aa72a6afa8fab264ccd3d750b298d1cd15184
Manoj Manike <43418679+DarkEyeX@users.noreply.github.com>
2023-07-17 13:27:29 +0200

    Update main.py

--- Mucb4.0/main.py
+++ Mucb4.0/main.py
@@ -38,34 +38,3 @@ plt.ylabel('Cumulative Regret')
 plt.legend()
 plt.show()
 
-'''
-num_simulations = 10
-
-avg_cumulative_regret = np.zeros(num_trials)
-avg_average_regret = np.zeros(num_trials)
-
-for _ in range(num_simulations):
-    ucb1 = MUCB(num_agents, num_trials, num_arms)
-    env = environment(num_arms, mu)
-    ucb1.run(env)
-    avg_cumulative_regret += ucb1.get_cum_rgt()
-    avg_average_regret += ucb1.get_avg_rgt()
-    ucb1.clear()
-
-avg_cumulative_regret /= num_simulations
-avg_average_regret /= num_simulations
-
-plt.figure(figsize=(10, 5))
-plt.plot(range(num_trials), avg_average_regret)
-plt.title('Average Regret Over Time')
-plt.xlabel('Time Step')
-plt.ylabel('Average Regret')
-plt.show()
-
-plt.figure(figsize=(10, 5))
-plt.plot(range(num_trials), avg_cumulative_regret)
-plt.title('Cumulative Regret Over Time')
-plt.xlabel('Time Step')
-plt.ylabel('Cumulative Regret')
-plt.show()
-'''
