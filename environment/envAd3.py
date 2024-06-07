6e08dcc6e972b1a66a2a34e596d50aba1c6facf3
René Hoppe <rene.hoppe-p1j@rub.de>
2024-06-07 14:46:05 +0200

    Add latest test end adv3.

--- environment/envAd3.py
+++ environment/envAd3.py
@@ -1,6 +1,6 @@
 """
 I've been creative: At any point in time, one arm is the best arm
-with reward 1, while the reward of any other arm is 1/d^2, where
+with reward 1, while the reward of any other arm is 1/(d+1)^2, where
 d is the distance to the best arm, assuming circular placement.
 Example for d:
 2 1 0 1 2 3 4 5 4 3
@@ -10,18 +10,29 @@ a random arm is assigned as the new best, with the others being refreshed accord
 """
 import numpy as np
 
-class env_adverse2:
+class env_adverse3:
 	def __init__(self,num_arm, noise=0, difficulty=20):
 		self.num_arm = num_arm
 		self.noise = noise
 		self.rng = np.random.default_rng()
 		self.difficulty = difficulty #For how many draws it stays stable.
+		self.progress = 0 #Triggers switch when reaching difficulty.
+		self.rewards = [0]*self.num_arm
 	
 	def feedback(self,arm):
-		golden_index = self.rng.integers(0, self.num_arm)
-		reward = 0
-		if arm == golden_index: #The only difference to env_adverse1.
-			reward = 1
+		self.progress += 1
+		if self.progress >= self.difficulty:
+			golden_index = self.rng.integers(1, self.num_arm)
+			self.rewards = [0]*self.num_arm
+			self.rewards[golden_index] = 1
+			for i in range(0, int(np.ceil(self.num_arm/2))+1):
+				for j in (-i, i):
+					if self.rewards[(golden_index+j)%self.num_arm] == 0:
+						self.rewards[(golden_index+j)%self.num_arm] = 1 / (i+1)**2
+			self.progress = 0
+			#print(self.rewards)
+			
+		reward = self.rewards[arm]
 		
 		# Does it make sense to add noise in a nonstochastic env?
 		# Not sure, so consider case where there is no noise.
@@ -35,7 +46,7 @@ class env_adverse2:
 
 """
 for i in [2,5,50]:
-	ad = env_adverse2(i)
+	ad = env_adverse3(i)
 	print(i, "Arms")
 	for j in range(0,30):
 		print(ad.feedback(0)[0], end=" ")
--- test/test5.py
+++ test/test5.py
@@ -13,6 +13,8 @@ sys.path.append('../')
 from environment.Gaussian_noise import Gaussian_noise as gn
 from environment.env import env_stochastic
 from environment.envAd1 import env_adverse1
+from environment.envAd2 import env_adverse2
+from environment.envAd3 import env_adverse3
 from algorithms.exp3 import exp3
 from algorithms.epsilion import EpsilonGreedy
 from algorithms.UCB1 import UCB1
@@ -21,7 +23,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 num_arm = 5
-T = 100
+T = 1000
 
 # Stochastic
 mu = np.array([0.1, 0.9, 0.4, 0.3, 0.6])
@@ -29,11 +31,14 @@ Trial = 10
 noise = gn(1,0,0.01,[-0.1,0.1])
 envs = list()
 envs.append(env_stochastic(num_arm, mu, noise))
+envs.append(env_adverse1(num_arm, noise))
+envs.append(env_adverse2(num_arm, noise))
+envs.append(env_adverse3(num_arm, noise, difficulty=100))
 
 
-algorithms = [exp3(T, num_arm, alpha=0.1, gamma=0.8), UCB1(T, num_arm)]
+algorithms = [exp3(T, num_arm, alpha=0.1, gamma=0.3), UCB1(T, num_arm)]
 algorithm_names = ['exp3', 'UCB1']
-env_names = ['stochastic']
+env_names = ['stochastic', 'biggerBetter', 'random', 'shifting']
 avg_regret = []
 cum_regret = []
 
