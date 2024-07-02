5fed577841a8f6a2fccebc3f57afdbe1bfe81dcb
René Hoppe <rene.hoppe-p1j@rub.de>
2024-07-02 18:06:45 +0200

    Compare the fixed BGD with the unfixed version.

--- test/testContinous.py
+++ test/testContinous.py
@@ -5,11 +5,12 @@ sys.path.append('../')
 from environment.envParabola import EnvParabola
 from algorithms.greedyProjection import GreedyProjection
 from algorithms.bgd import BGD
+from algorithms.bgdOld import BGD2
 
 
 
-T = 100
-repeats = 10
+T = 500
+repeats = 30
 
 envs = list()
 envs.append(EnvParabola(dimensions=3, pos_mean=0, pos_scale=5, slope_mean=1, slope_scale=0.5, boundaries=10, stability=1))
@@ -17,8 +18,8 @@ envs.append(EnvParabola(dimensions=3, pos_mean=0, pos_scale=5, slope_mean=1, slo
 envs.append(EnvParabola(dimensions=3, pos_mean=0, pos_scale=5, slope_mean=1, slope_scale=0.5, boundaries=10, stability=0))
 
 
-algorithms = [GreedyProjection(T), BGD(T)]
-algorithm_names = ["Greedy Projection", "BGD"]
+algorithms = [BGD2(T), BGD(T)]
+algorithm_names = ["Unfixed BGD", "Fixed BGD"]
 env_names = ["fully stable", "less stable", "unstable"]
 
 test(T, repeats, envs, algorithms, algorithm_names, env_names)
