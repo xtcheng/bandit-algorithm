da564a9202eeb75443740597dad4f93535812e9a
René Hoppe <rene.hoppe-p1j@rub.de>
2024-11-05 11:14:14 +0100

    Including the unsuited figures

--- algorithms/modular/basicMultiObjective.py
+++ algorithms/modular/basicMultiObjective.py
@@ -17,8 +17,12 @@ class BasicMultiObjective(AbstractMAB):
 	
 	# run needs to be redifined because of the reward being multi-dimensional and there is no optimal action, but an optimal mix.
 	def run(self,env):
-		total = 0 # Total costs
-		total_mix = 0 # Sum of all alphas the selection module uses over time.
+		total1 = 0 # Total costs since start
+		total2 = 0 # Total costs since start or breakpoint
+		total_mix1 = 0 # Sum of all alphas the selection module uses over time.
+		total_mix2 = 0 # Sum of all alphas the selection module uses over time, since start or breakpoint.
+		since_break = 0 # steps since start or breakpoint
+		
 		for t in range(0,self.T):
 			arm = self.selection_module.suggestArm()
 			reward, optimal_costs = env.feedback(arm)
@@ -28,11 +32,23 @@ class BasicMultiObjective(AbstractMAB):
 			
 			# For performance analysis
 			
+			# Breakpoint?
+			if env.turns in env.breakpoints:
+				total2 = 0
+				total_mix2 = 0
+				since_break = 0
+			since_break += 1
+			
 			# Regret
-			total += reward
+			total1 += reward
+			total2 += reward
+			
 			ar = [self.weights]
-			ar.append(total / (t+1))
-			gini_avg = gini([1], ar)
+			ar.append(total1 / (t+1))
+			gini_avg1 = gini([1], ar)
+			
+			ar[-1] = total2 / since_break
+			gini_avg2 = gini([1], ar)
 			
 			
 			# Pseudo regret
@@ -41,8 +57,10 @@ class BasicMultiObjective(AbstractMAB):
 				# Of course you must not use such direct access outside the analysis.
 				ar.append(m)
 			# Keep track of how the alpha developes
-			total_mix += self.selection_module.current_mix
-			pseudo = gini(total_mix / (t+1), ar)
+			total_mix1 += self.selection_module.current_mix
+			total_mix2 += self.selection_module.current_mix
+			pseudo1 = gini(total_mix1 / (t+1), ar)
+			pseudo2 = gini(total_mix2 / since_break, ar)
 			
 			# Instantaneous regret
 			ins = gini(self.selection_module.current_mix, ar) - optimal_costs
@@ -51,17 +69,32 @@ class BasicMultiObjective(AbstractMAB):
 			self.sum_rgt += ins
 			self.avg_rgt[t] = self.sum_rgt / (t+1)
 			self.cum_rgt[t] = self.sum_rgt
-			self.psd_rgt[t] = pseudo - optimal_costs
+			self.psd_rgt1[t] = pseudo1 - optimal_costs
+			self.psd_rgt2[t] = pseudo2 - optimal_costs
+			self.rgt1[t] = gini_avg1 - optimal_costs
+			self.rgt2[t] = gini_avg2 - optimal_costs
 		#print(self.selection_module.mu)
 		#print("Settled on this mix:")
 		#print(self.selection_module.current_mix)
 	
 	
-	def get_psd_rgt(self):
-		return self.psd_rgt
+	def get_psd_rgt1(self):
+		return self.psd_rgt1
+	
+	def get_psd_rgt2(self):
+		return self.psd_rgt2
+	
+	def get_rgt1(self):
+		return self.rgt1
+	
+	def get_rgt2(self):
+		return self.rgt2
 	
 	
 	def clear(self):
 		# Also set the Pseudo regret because we have it.
-		self.psd_rgt = [0]*self.T
+		self.psd_rgt1 = [0]*self.T
+		self.psd_rgt2 = [0]*self.T
+		self.rgt1 = [0]*self.T
+		self.rgt2 = [0]*self.T
 		super().clear()
--- test/masterTester.py
+++ test/masterTester.py
@@ -5,8 +5,10 @@ def test(T, repeats, envs, algorithms, algorithm_names, env_names):
 
 	avg_regret = []
 	cum_regret = []
-	psd_regret = []
-	has_pseudo = False
+	psd_regret1 = []
+	psd_regret2 = []
+	regret1 = []
+	regret2 = []
 	sum_times = [0]*len(algorithms)
 	
 	assert len(envs) == len(env_names) and len(algorithms) == len(algorithm_names)
@@ -15,7 +17,10 @@ def test(T, repeats, envs, algorithms, algorithm_names, env_names):
 		for i, algorithm in enumerate(algorithms):
 			cum_regret.append([0]*T)
 			avg_regret.append([0]*T)
-			psd_regret.append([0]*T)
+			psd_regret1.append([0]*T)
+			psd_regret2.append([0]*T)
+			regret1.append([0]*T)
+			regret2.append([0]*T)
 			start_time = time.perf_counter()
 			for trial in range(repeats):
 				algorithm.clear()
@@ -25,9 +30,10 @@ def test(T, repeats, envs, algorithms, algorithm_names, env_names):
 				for y in range(T):
 					cum_regret[-1][y] += algorithm.get_cum_rgt()[y] / repeats
 					avg_regret[-1][y] += algorithm.get_avg_rgt()[y] / repeats
-					if hasattr(algorithm, "get_psd_rgt"):
-						psd_regret[-1][y] += algorithm.get_psd_rgt()[y] / repeats
-						has_pseudo = True
+					psd_regret1[-1][y] += algorithm.get_psd_rgt1()[y] / repeats
+					psd_regret2[-1][y] += algorithm.get_psd_rgt2()[y] / repeats
+					regret1[-1][y] += algorithm.get_rgt1()[y] / repeats
+					regret2[-1][y] += algorithm.get_rgt2()[y] / repeats
 			end_time = time.perf_counter()
 			sum_times[i] += end_time - start_time
 	
@@ -45,15 +51,23 @@ def test(T, repeats, envs, algorithms, algorithm_names, env_names):
 	#plt.show()
 	
 	
-	if has_pseudo:
-		plt.figure(figsize=(6, 5))
-		for j, env in enumerate(envs):
-			for i, algorithm in enumerate(algorithms):
-				plt.plot(range(T), psd_regret[len(algorithms)*j + i], label=algorithm_names[i]+" on "+env_names[j])
-		plt.xlabel('t (Trials)', fontsize=15)
-		plt.ylabel('Pseudo Regret', fontsize=15)
-		plt.legend(loc='upper right')
-		plt.title('Pseudo Regret')
+	plt.figure(figsize=(6, 5))
+	for j, env in enumerate(envs):
+		for i, algorithm in enumerate(algorithms):
+			plt.plot(range(T), psd_regret1[len(algorithms)*j + i], label=algorithm_names[i]+" on "+env_names[j])
+	plt.xlabel('t (Trials)', fontsize=15)
+	plt.ylabel('Pseudo Regret 1', fontsize=15)
+	plt.legend(loc='upper right')
+	plt.title('Pseudo Regret 1')
+	
+	plt.figure(figsize=(6, 5))
+	for j, env in enumerate(envs):
+		for i, algorithm in enumerate(algorithms):
+			plt.plot(range(T), psd_regret2[len(algorithms)*j + i], label=algorithm_names[i]+" on "+env_names[j])
+	plt.xlabel('t (Trials)', fontsize=15)
+	plt.ylabel('Pseudo Regret 2', fontsize=15)
+	plt.legend(loc='upper right')
+	plt.title('Pseudo Regret 2')
 	
 	
 	plt.figure(figsize=(6, 5))
@@ -64,4 +78,26 @@ def test(T, repeats, envs, algorithms, algorithm_names, env_names):
 	plt.ylabel('Average Regret', fontsize=15)
 	plt.legend(loc='upper right')
 	plt.title('Average Regret')
+	plt.show()
+	
+	
+	plt.figure(figsize=(6, 5))
+	for j, env in enumerate(envs):
+		for i, algorithm in enumerate(algorithms):
+			plt.plot(range(T), regret1[len(algorithms)*j + i], label=algorithm_names[i]+" on "+env_names[j])
+	plt.xlabel('t (Trials)', fontsize=15)
+	plt.ylabel('\"Regret\" 1', fontsize=15)
+	plt.legend(loc='upper right')
+	plt.title('\"Regret\" 1')
+	plt.show()
+	
+	
+	plt.figure(figsize=(6, 5))
+	for j, env in enumerate(envs):
+		for i, algorithm in enumerate(algorithms):
+			plt.plot(range(T), regret2[len(algorithms)*j + i], label=algorithm_names[i]+" on "+env_names[j])
+	plt.xlabel('t (Trials)', fontsize=15)
+	plt.ylabel('\"Regret\" 2', fontsize=15)
+	plt.legend(loc='upper right')
+	plt.title('\"Regret\" 2')
 	plt.show()
